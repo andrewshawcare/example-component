f8d893211aef12c351fd29dd83fd166c48238c8c
Andrew Shaw Care <andrewshawcare@gmail.com>
2016-05-22 19:47:18 -0500

    Refactored to use component workshop

--- spec.js
+++ spec.js
@@ -1,5 +1,7 @@
-describe("Example Component", function () {
-  it("passes a test", function () {
-    expect(true).toBe(true);
+define(["./index.js", function (ExampleComponent) {
+  describe("Example Component", function () {
+    it("passes a test", function () {
+      expect(true).toBe(true);
+    });
   });
-});
+}]);
